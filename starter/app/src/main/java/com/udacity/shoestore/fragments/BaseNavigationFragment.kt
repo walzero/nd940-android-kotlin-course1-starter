f3e299dffd3ac7c18dcec406400bc7f046390194
Walter J Rezende <walter-zero@hotmail.com>
2020-12-24 18:34:39 -0300

    Removed unused import

--- starter/app/src/main/java/com/udacity/shoestore/fragments/BaseNavigationFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/fragments/BaseNavigationFragment.kt
@@ -1,11 +1,7 @@
 package com.udacity.shoestore.fragments
 
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.udacity.shoestore.R
 
 abstract class BaseNavigationFragment : Fragment() {
     open fun popCurrentFragment() = findNavController().popBackStack()
